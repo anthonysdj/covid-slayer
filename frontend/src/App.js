f9e94de145e5f7cb9ffdea5a0f13e311d9fd1a02
tons <anthony_dejesus@worldvision.org.ph>
2020-08-06 00:12:52 +0800

    change network name and added some components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import LoginContainer from './components/login/LoginContainer';
 
 function App() {
     return (
         <div className="App">
-            Hilu
+            <LoginContainer></LoginContainer>
         </div>
     );
 }
